8fd14b018cfb09d0e2379fc2e40c6f93cf8cfd84
MikaelMR <mikaramos.pro@gmail.com>
2023-04-01 18:59:53 -0300

    Design Inicial

---
+++ resources/views/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculadora Laravel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+
+<body style="background-color: #dddddd">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-4 m-auto">
+                <div class="card" style="background-color: #5f5f5f">
+                    <div class="card-body m-auto">
+                        <h1 class="text-center" style="color: #dddddd">Calculadora</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
